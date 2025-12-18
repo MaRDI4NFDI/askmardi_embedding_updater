565e4bbda30fbf885e251d96405274c98ca36d51
Tim Conrad <conrad@zib.de>
2025-12-18 11:41:52 +0100

    Logger now accepts a custom log layout

--- helper/logger.py
+++ helper/logger.py
@@ -8,19 +8,24 @@ _LOG_FORMAT = (
     "[thread=%(threadName)s id=%(thread)d] | %(message)s"
 )
 
-def get_logger_safe() -> logging.Logger:
+def get_logger_safe( logformat: str = None )  -> logging.Logger:
     """Return an available logger for Prefect or local execution."""
     try:
         # Prefect logger – do NOT touch handlers or formatters
         return get_run_logger()
 
     except MissingContextError:
+        if logformat is not None:
+            logformat_to_use = logformat
+        else:
+            logformat_to_use = _LOG_FORMAT
+
         logger = logging.getLogger(__name__)
         logger.setLevel(logging.INFO)
 
         if not logger.handlers:
             handler = logging.StreamHandler()
-            handler.setFormatter(logging.Formatter(_LOG_FORMAT))
+            handler.setFormatter(logging.Formatter(logformat_to_use))
             logger.addHandler(handler)
 
         logger.propagate = False
